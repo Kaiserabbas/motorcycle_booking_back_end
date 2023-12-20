d47fc49381fd6613c951a1aa32adb26ae577588c
graciano1997 <gracianomanuelhenrique@gmail.com>
2023-12-20 16:43:25 +1400

    add create reserve functionality

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,9 +1,25 @@
 class ReservationsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+
   def index; end
 
   def show; end
 
-  def create; end
+  def create
+    reservation=Reservation.new(reservation_params)
+    reservation.user=current_user
+    if (reservation.save)
+       render json: { success: true, message: 'Created Successfully!' }
+     elsif
+       render json: { error: true, message: 'Ups! Could not Create the new Reservation' }
+     end
+  end
 
   def destroy; end
+
+  private
+
+   def reservation_params
+     params.require(:reservation).permit(:duration, :total, :motorcycle_id, :date, :city)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-   resources :reservations, default:{format: :json}
+   resources :reservations, default: {format: :json}, except:[:update]
   resources :motorcycles, default: {format: :json}, except:[:update]
   get "up" => "rails/health#show", as: :rails_health_check
   # Defines the root path route ("/")
