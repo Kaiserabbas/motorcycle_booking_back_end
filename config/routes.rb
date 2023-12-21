8e52b9231c1b2246ecd988df087208fa38b93e4e
Qaisar <kayser.abbas@gmail.com>
2023-12-21 16:16:34 +0500

    Update route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :reservations, default: { format: :json }, except: [:update]
-  resources :motorcycles, default: { format: :json }, except: [:update]
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
-      post '/login', to: 'auth#login'
+      resources :reservations, default: { format: :json }, except: [:update], only: [:index, :create, :show, :destroy]
+      resources :motorcycles, default: { format: :json }, except: [:update], only: [:index, :show, :create, :destroy]
+      post '/login', to: 'authentication#login'
     end
   end
   # get "up" => "rails/health#show", as: :rails_health_check
