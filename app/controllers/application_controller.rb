f2a4bbfadc845e8c39ec1a9c31f977af37f13680
Qaisar <kayser.abbas@gmail.com>
2023-12-21 16:15:42 +0500

    add protect_from_forgery method to skip CSRF protection for API requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+
   def authorize_request
     header = request.headers['Authorization']
     header = header.split.last if header
