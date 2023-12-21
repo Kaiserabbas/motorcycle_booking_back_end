89b80c1e23d4347b4f38f93b03700d3f6eb3191d
Qaisar <kayser.abbas@gmail.com>
2023-12-21 12:51:17 +0500

    Update user model to handle authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
   has_many :reservations, dependent: :destroy
+  has_many :motorcycles, through: :reservations
+
   validates_presence_of :name, :email
+  validates_presence_of :password, length: {minimum:6, maximum:12}, allow_nil: true
+
+  has_secure_password
+
+  def generate_jwt
+    JWT.encode({id: id, exp: 2.days.from_now.to_i}, Rails.application.secrets.secret_key_base)
+  end
+
+  def self.form_jwt(token)
+    JWT.decode(token, Rails.application.secrets.secret_key_base)[0]['id']
+  end
 end
