183ef45ef4bb6b9a66558585609582b6c285c8a7
Zel-hub7 <zelalemyohannes75@gmail.com>
2024-01-12 11:57:10 +0100

    remove useless comments

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,16 +39,6 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
-  # You can uncomment this line to turn off ActiveRecord support entirely.
-  # config.use_active_record = false
-
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
   #
   #     RSpec.describe UsersController, type: :controller do
   #       # ...
